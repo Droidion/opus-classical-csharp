2ee449927e3ccc0cfa43570e9a31321bc86e2714
Denis Rodionov <nicholas.hawkwood@gmail.com>
2024-08-17 23:58:16 +0300

    del unused

--- OpusClassical/Program.cs
+++ OpusClassical/Program.cs
@@ -1,4 +1,3 @@
-using Blazored.LocalStorage;
 using OpusClassical.Components;
 using OpusClassical.Models;
 using OpusClassical.Repositories;
@@ -26,8 +25,6 @@ var supabase = new Client(envConfig.SupabaseUrl, envConfig.SupabaseKey, new Supa
 });
 
 builder.Services.AddSingleton(supabase);
-
-builder.Services.AddBlazoredLocalStorage();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
